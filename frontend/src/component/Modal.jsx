f7ee8cd2f719087f9229994d0929f60b959ef491
Ananda Ferreira <ananda.ferreiragomes@gmail.com>
2025-01-15 15:42:11 +0100

    style modal

--- frontend/src/component/Modal.jsx
+++ frontend/src/component/Modal.jsx
@@ -19,29 +19,34 @@ export default function Modal({ project, onClose }) {
   return ReactDom.createPortal(    
     <div className="modal-overlay">
         <div className="modal-content">
-            <div className="modal-grid">       
-                <div className="modal-gallery">
-                    {project.images?.length > 0 ? (
-                        <>
-                            <button className="nav-button left" onClick={prevImage}>&lt;
-                            </button>
-                            <img src={project.images[currentImageIndex].asset.url} alt={project.title} className="modal-gallery-image" />
-                            <button className="nav-button right" onClick={nextImage}>&gt;                                
-                            </button>
-                        </>
-                    ) : (
-                        <p>No images available</p>
-                    )}
-                </div>
-
-                <div className="modal-info">
+            {project.images && <div className="modal-gallery">
+                {project.images.length > 1 && 
+                    <button className="nav-button left" onClick={prevImage}>&lt;</button> }
+                <img src={project.images[currentImageIndex].asset.url} alt={project.title} className="modal-gallery-image" />
+                {project.images.length > 1 && 
+                    <button className="nav-button right" onClick={nextImage}>&gt;</button> }
+                {/* {project.images?.length > 0 ? (
+                    <>
+                        <button className="nav-button left" onClick={prevImage}>&lt;
+                        </button>
+                        <img src={project.images[currentImageIndex].asset.url} alt={project.title} className="modal-gallery-image" />
+                        <button className="nav-button right" onClick={nextImage}>&gt;                                
+                        </button>
+                    </>
+                ) : (
+                    <p>No images available</p>
+                )} */}
+            </div>}
+            <div className="modal-info" style={{width: project.images ? '' : '100%'}}>
+                <div className="text"> 
                     <h3 className="title">{project.title}</h3>
                     <h4 className="subtitle">{project.extendedsubtitle}</h4>
                     <a href={project.externalLink} target="_blank" rel="noopener noreferrer">Follow link</a>
                     <div className="description"><BlockContent blocks={project.description} projectId="f588b6e1" dataset="production" /></div>
-                    <button className="close-button" onClick={onClose}>Back</button>
                 </div>
-            </div>      
+                <button className="close-button" onClick={onClose}>Back</button>
+            </div>
+
         </div>    
     </div>,
     document.getElementById('portal')
