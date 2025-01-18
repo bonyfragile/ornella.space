6a243883c7c3c0f71fe5f855686276fdb210b956
bonyfragile <ornella.pacchioni@hotmail.com>
2025-01-18 16:51:25 +0100

    favicon and styles

--- frontend/src/component/Modal.jsx
+++ frontend/src/component/Modal.jsx
@@ -46,7 +46,7 @@ export default function Modal({ project, onClose }) {
             <div className="modal-info" style={{width: project.images ? '' : '100%'}}>
                     <h3 className="title">{project.title}</h3>
                     <h4 className="subtitle">{project.extendedsubtitle}</h4>
-                    <a href={project.externalLink} target="_blank" rel="noopener noreferrer">Follow link</a>
+                    <a href={project.externalLink} target="_blank" rel="noopener noreferrer">{project.externalLink}</a>
                     <div className="description"><BlockContent blocks={project.description} projectId="f588b6e1" dataset="production" /></div>
                 <button className="close-button" onClick={onClose}>Back</button>
             </div>
